6d142c67017e1acd3c55edfffdb085b98a4e892b
xrakovsky <xrakovsky@stuba.sk>
2020-03-28 15:11:41 +0100

    rm

--- pac-man/src/StateManager/State.ts
+++
@@ -1,9 +0,0 @@
-import { ObjectManager } from "../ObjectManager/ObjectManager";
-
-export class State {
-    protected objects: ObjectManager[] = [];
-
-    public render(context: CanvasRenderingContext2D) {
-        this.objects.forEach(element => element.render(context));
-    }
-}
