2208b0fbf7033387a2dcf10cb19c4e9d4ef2e3b9
xrakovsky <xrakovsky@stuba.sk>
2020-03-28 02:18:28 +0100

    delete

--- pac-man/src/Game/StateManager/State.ts
+++
@@ -1,9 +0,0 @@
-import { Objects } from "../ObjectManager/Objects";
-
-export class State {
-    protected objects: Objects[] = [];
-
-    public render(context: CanvasRenderingContext2D) {
-        this.objects.forEach(object => object.render(context));
-    }
-}
--- pac-man/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Game } from "./Game/Game";
-
-const game = new Game();
-
-game.start().catch(err => console.error(err));
